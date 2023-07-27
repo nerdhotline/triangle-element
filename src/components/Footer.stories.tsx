f9237ae45b8d19e100dc0b7bfa723b72038f2b26
zmadvero <za366@drexel.edu>
2023-07-27 13:08:33 -0400

    clean up footer component a bit

--- src/components/Footer.stories.tsx
+++ src/components/Footer.stories.tsx
@@ -3,12 +3,9 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Footer } from './Footer';
 
 const meta = {
-  title: 'Example/Footer',
+  title: 'Components/Footer',
   component: Footer,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ['autodocs'],
   parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'fullscreen',
   },
 } satisfies Meta<typeof Footer>;
