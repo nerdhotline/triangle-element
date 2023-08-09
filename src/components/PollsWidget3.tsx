36547bee402148ca773322fce9eb218061b3af52
nerdhotline <79532110+nerdhotline@users.noreply.github.com>
2023-08-09 22:47:58 +0000

    Edited Polls Widget.

--- src/components/PollsWidget3.tsx
+++ src/components/PollsWidget3.tsx
@@ -7,28 +7,22 @@ export const PollsWidget3 = (_props: {}) => {
 
             <div id="polls-173-ans" className="wp-polls-ans">
                 <ul className="wp-polls-ul">
-                    <li>consectetur
-                        <small>(40%)</small>
+                    <li><strong>Consectetur <small>(40%)</small></strong>
                         <div className="pollbar" style = {{"width": "40%"}} title="Saxbys (40%)"></div>
                     </li>
-                    <li>adipiscing elit
-                        <small>(31%)</small>
+                    <li>Adipiscing elit <small>(31%)</small>
                         <div className="pollbar" style = {{"width": "31%"}} title="Board and Brew (31%)"></div>
                     </li>
-                    <li>sed do eiusmod 
-                        <small>(12%)</small>
+                    <li>Sed do eiusmod <small>(12%)</small>
                         <div className="pollbar" style = {{"width": "12%"}} title="Green Line Cafe (12%)"></div>
                     </li>
-                    <li>tempor incididunt
-                        <small>(10%)</small>
+                    <li>Tempor incididunt <small>(10%)</small>
                         <div className="pollbar" style = {{"width": "10%"}} title="You Have Voted For This Choice - Greek From Greece (10%)"></div>
                     </li>
-                    <li>labore dolore
-                        <small>(4%)</small>
+                    <li>Labore dolore <small>(4%)</small>
                         <div className="pollbar" style = {{"width": "4%"}} title="Elixir (4%)"></div>
                     </li>
-                    <li>magna aliqua
-                        <small>(4%)</small>
+                    <li>Magna aliqua <small>(4%)</small>
                         <div className="pollbar" style = {{"width": "4%"}} title="Arterial Coffee (4%)"></div>
                     </li>
                 </ul>
